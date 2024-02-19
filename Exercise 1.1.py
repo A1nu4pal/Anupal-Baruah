02f6c80726ee69f33e83d15f69700428556d2ced
Anupal Baruah <160456530+A1nu4pal@users.noreply.github.com>
2024-02-19 20:51:23 +0530

    Add files via upload

---
+++ Exercise 1.1.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Question 1:
+# SyntaxError: unexpected EOF while parsing
+print("Hello, World!"  
+  Cell In[2], line 3
+    print("Hello, World!"
+                           
+
+
+# In[2]:
+
+
+# Question 2:
+# SyntaxError: invalid syntax
+print(Hello, World!)  
+  Cell In[3], line 3
+    print(Hello, World!)
+
+
+# In[3]:
+
+
+# Question 3:
+# Output: 4
+# Example:
+print(+2)  # Output: 2
+print(2++2)  # Output: 4
+2
+4
+
+
+# In[4]:
+
+
+# Question 4:
+# SyntaxError: leading zeros in decimal integer literals are not permitted; use an 0o prefix for octal integers
+print(09)  
+  Cell In[7], line 3
+    print(09)
+
+
+# In[5]:
+
+
+# Question 5:
+# SyntaxError: invalid syntax
+print(2 3)  
+  Cell In[8], line 3
+    print(2 3)
+
+
+# In[ ]:
+
+
+
+
